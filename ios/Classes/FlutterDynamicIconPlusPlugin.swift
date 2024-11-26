6ddf83b65e475ae8c27e85e3605ed42102e9d0f0
Aleksandr Popov (Eyewa) <aleksandrpopoveyewa@Aleksandrs-MacBook-Pro.local>
2024-11-26 18:43:55 +0100

    feat: Support app icon reset when `isSilent` is true

--- ios/Classes/FlutterDynamicIconPlusPlugin.swift
+++ ios/Classes/FlutterDynamicIconPlusPlugin.swift
@@ -141,14 +141,13 @@ public class FlutterDynamicIconPlusPlugin: NSObject, FlutterPlugin {
     }
     
     private func setIconWithoutAlert(_ appIcon: String?, result: @escaping FlutterResult) {
-        guard let appIcon = appIcon else { return }
         if UIApplication.shared.responds(to: #selector(getter: UIApplication.supportsAlternateIcons)) && UIApplication.shared.supportsAlternateIcons {
-            typealias setAlternateIconName = @convention(c) (NSObject, Selector, NSString, @escaping (NSError) -> ()) -> ()
+            typealias setAlternateIconName = @convention(c) (NSObject, Selector, NSString?, @escaping (NSError) -> ()) -> ()
             let selectorString = "_setAlternateIconName:completionHandler:"
             let selector = NSSelectorFromString(selectorString)
             let imp = UIApplication.shared.method(for: selector)
             let method = unsafeBitCast(imp, to: setAlternateIconName.self)
-            method(UIApplication.shared, selector, NSString(string: appIcon), { _ in })
+            method(UIApplication.shared, selector, appIcon as NSString?, { _ in })
         } else {
             result(nil)
         }
